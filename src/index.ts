ef67be0694131adf2f15ec189d095c388f13bf27
Yash kandiyal <yashkandiyal22@gmail.com>
2024-08-02 19:35:56 +0530

    webhook-handler-changes.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 import express from "express";
 import connectDB from "./config/db";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { createServer } from "http";
 import { initializeSocket } from "./socket";
 import { FRONTEND_URL, PORT } from "./config/env";
+import { webhookHandler } from "./utils/webhookHandler";
+
 const app = express();
 
 const frontendUrl = FRONTEND_URL;
 
 // Middleware
-app.use(cors({ origin: frontendUrl }));
+app.use(
+  cors({
+    origin: frontendUrl,
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
 connectDB();
+
 app.get("/", (req, res) => {
   res.send("API running!");
 });
+app.post("/api/webhooks", webhookHandler);
 
 const server = createServer(app);
 initializeSocket(server);
+
 server.listen(PORT, () => {
   console.log(`server running at port http://localhost:${PORT}`);
 });
+
 export default app;
